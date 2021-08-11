d4eebf909f3ec996a891dd5ccd3e9ff417c27cf6
Adrian Moya <amoya6@gatech.edu>
2021-08-11 01:55:16 +0000

    added redirect logic

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.urlencoded({extended: true}));
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/top-level.html'))
+})
+
+app.get('/home', (req, res) => {
+  res.send('hello');
+})
+
+app.post('/set-cookie-and-redirect', (req, res) => {
+  const { redirectTo, cookieName, cookieValue } = req.body;
+  res.cookie(cookieName, cookieValue);
+  res.redirect(redirectTo || '/home');
 })
 
 app.listen(1234)
